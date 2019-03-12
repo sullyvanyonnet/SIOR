60e4650061464a23027cafe44d131aa2f3a71af8
ubo <ubo@ubo-VirtualBox>
2019-03-12 09:35:07 +0100

    et on recommit

---
+++ projet1/CRUDs/CRUDMariadb.js
@@ -0,0 +1,46 @@
+const mariadb = require('mariadb');
+
+function connect (user, mdp, base, hote){
+	return mariadb.createConnection({
+      		host: hote,
+      		user : user,
+			//port: 3306,
+	  		password : mdp,
+	  		database : base
+   		})
+}
+//module.exports = connect;
+
+module.exports = {
+	executeSelect : function (db, sql, res, result, func){
+			connect(db[0], db[1], db[2], db[3]).then(conn => {	
+				conn.query(sql)
+					.then((rows) => {
+						//console.log(rows);
+						result = rows;
+						func(res, result);
+					})
+					.catch(err => {
+						console.log("requete echouée: " + err);
+					});
+			})
+			.catch(err => {
+				console.log("not connected due to error: " + err);
+			});
+		},
+
+	executeUpdate : function (db, sql) {
+			connect(db[0], db[1], db[2], db[3]).then(conn => {	
+				conn.query(sql)
+					.then((rows) => {
+						console.log(rows);
+					})
+					.catch(err => {
+						console.log("requete echouée: " + err);
+					});
+			})
+			.catch(err => {
+				console.log("not connected due to error: " + err);
+			});
+		}
+}
---
+++ projet1/CRUDs/CRUDMongodb.js
@@ -0,0 +1,61 @@
+const MongoClient = require('mongodb').MongoClient;
+
+
+module.exports = class MongoDB {
+	constructor (hote, base) {
+		this.base = base;
+		this.hote = hote;
+	}
+	
+	executeSelect(col, rqt, callback) {
+		console.log(rqt);
+		let base = this.base;
+		MongoClient.connect("mongodb://"+this.hote, {useNewUrlParser: true}, function(error, client) {
+			if (error) throw error;
+			var db = client.db(base);
+			db.collection(col).find(rqt, {voy_images:true}).toArray(function(err, items) {
+				 return callback(items);
+             });
+			 client.close();
+		});
+	}
+	
+	executeUpdate(col, myQuery, newValues, res,  callback){
+		let base = this.base;
+		MongoClient.connect("mongodb://"+this.hote, {useNewUrlParser: true}, function(error, client) {
+			if (error) throw error;
+			var db = client.db(base);
+			db.collection(col).updateOne(myQuery, newValues, function(err, items) {
+				if (err) throw err;
+				return callback(res, items);
+			});
+			client.close();
+		});
+	}
+	
+	executeDeleteOne(col, myQuery, res,  callback){
+		let base = this.base;
+		MongoClient.connect("mongodb://"+this.hote, {useNewUrlParser: true}, function(error, client) {
+			if (error) throw error;
+			var db = client.db(base);
+			db.collection(col).deleteOne(myQuery, function(err, items) {
+				if (err) throw err;
+				return callback(res, items);
+			});
+			client.close();
+		});
+	}
+	
+	executeInsertOne(col, myObj, res,  callback){
+		let base = this.base;
+		MongoClient.connect("mongodb://"+this.hote, {useNewUrlParser: true}, function(error, client) {
+			if (error) throw error;
+			var db = client.db(base);
+			db.collection(col).insertOne(myObj, function(err, items) {
+				if (err) throw err;
+				return callback(res, items);
+			});
+			client.close();
+		});
+	}
+}
