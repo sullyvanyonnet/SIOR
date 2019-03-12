ccc24b649e68e54a4d40c843da7d4ceb0807b92f
ubo <ubo@ubo-VirtualBox>
2019-03-12 09:31:04 +0100

    modif

--- projet1/src/connection.jsx
+++ projet1/src/connection.jsx
@@ -10,19 +10,29 @@ class Connection extends React.Component {
             login: ""
         }
         this.enregistrer = this.enregistrer.bind(this)
+        this.annuler = this.annuler.bind(this)
+
         this.handlechange = this.handlechange.bind(this)
 
 
     }
   enregistrer(){
-        console.log(this.state); 
-        console.log('enregistrer ' + JSON.stringify(this.state));
+
         this.props.handler(<h2>bonjour {this.state.login} et ton mdp est {this.state.password}</h2>,1)
         /*axios.post('ServletConnection', this.state.login , this.state.password)
             .then(res => {
                 console.log(JSON.stringify(res.data));
             })*/
   }
+
+    annuler(){
+        this.props.handler("",0)
+        /*axios.post('ServletConnection', this.state.login , this.state.password)
+            .then(res => {
+                console.log(JSON.stringify(res.data));
+            })*/
+  }
+
   handlechange(event){
     const target = event.target;
     const value = target.value;
@@ -52,11 +62,11 @@ class Connection extends React.Component {
                 </tr>
                 <tr>
                     <td>mot de passe :</td>
-                    <td><input type="text" name="password" onChange={this.handlechange}/></td>
+                    <td><input type="password" name="password" onChange={this.handlechange}/></td>
                 </tr>
                 <tr>
                     <td>
-                        <button className="btn btn-default" onClick={this.enregistrer}>
+                        <button className="btn btn-default" onClick={this.annuler}>
                             annuler
                         </button>
                     </td>
