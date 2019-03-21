48d3fefe544d79d70e4b439a328641c9c554c58c
ubo <ubo@ubo-VirtualBox>
2019-03-21 09:11:45 +0100

    librairies

--- projet1/server.js
+++ projet1/server.js
@@ -220,7 +220,39 @@ function getCountPanierClientCallback(res, result) {
 }
 
 
+app.get('/api/confirmePanierClient', function (req, res) {
+    console.log('/confirmePanierClient');
 
+    let res_id = req.query.res_id || 0;
+    let cli_id = req.query.cli_id || 0;
+    let voy_id = req.query.voy_id || 0;
+
+
+    let MongoDB = new mongodb('obiwan2.univ-brest.fr', 'Assurance');
+    let rows = null;
+    var myobj = {
+        "res_id": res_id,
+        "cli_id": cli_id,
+        "voy_id": voy_id,
+        "date": "new Date()"
+    };
+
+    MongoDB.executeInsertOne("historique", myobj, function (result) {
+        rows = result;
+        console.log(rows);
+    });
+
+    let sql = `Delete from Reservation where res_id = ` + res_id + `;`;
+
+    console.log(sql);
+    MariaDB.executeSelect(db, sql, res, confirmePanierClientCallback);
+});
+
+function confirmePanierClientCallback(res, result) {
+    console.log(result);
+
+    res.send(result);
+}
 
 
 
